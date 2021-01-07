6b39e5d1760511dd3c0edeadcef0dff2cdd82663
qam6227 <qam6227@rit.edu>
2021-01-07 12:47:40 -0500

    added a different audio  component

--- src/Epsiodes.js
+++ src/Epsiodes.js
@@ -1,9 +1,37 @@
 import React from 'react'; 
-import Audio from 'react-audio-player'; 
-import mp3 from './Doridori(Instrumental).mp3'; 
-import hp3 from './Hunter x Hunter - Departure (Instrumental) (TV Size).mp3'; 
+import Audio from 'react-h5-audio-player';
+import DoriDori from './Doridori (Instrumental).mp3'; 
+import 'react-h5-audio-player/lib/styles.css'; 
+import Departure  from './Hunter x Hunter - Departure (Instrumental) (TV Size).mp3'; 
 
-class Episodes extends React.Component {
+const playlist = [ 
+    { name: 'DoriDori (Pokemon X & Y Ending)' , src: DoriDori}, 
+    { name: 'Departure (Hunter X Hunter Opening)',  src: Departure}, 
+]
+
+interface PlayListState { 
+    currentMusicIndex: number
+}
+
+class Episodes extends React.Component <null,PlayListState>{
+    constructor(props){
+        super(props); 
+        this.state = { 
+            currentMusicIndex: 0
+        }
+    }
+
+    handleClickPrevious = (): void => {
+    this.setState((prevState) => ({
+      currentMusicIndex: prevState.currentMusicIndex === 0 ? playlist.length - 1 : prevState.currentMusicIndex - 1,
+    }))
+  }
+
+  handleClickNext = (): void => {
+    this.setState((prevState) => ({
+      currentMusicIndex: prevState.currentMusicIndex < playlist.length - 1 ? prevState.currentMusicIndex + 1 : 0,
+    }))
+  }
    
 
 
@@ -18,14 +46,16 @@ class Episodes extends React.Component {
                 about the craziest things in football. So come have a seat and indulge in the some idiots 
                 having fun bonding over the insanity that is the National Football League.</p>
                 
-                <h2>Newest Episode</h2> 
-                <Audio src = {mp3} controls /> 
-
-                <h2>Last Epsiode</h2>
-                <Audio src = {hp3} controls />
-
-                <h2>Previous Episodes</h2>
-
+                <Audio
+                    header = {"Now playing " + [this.state.currentMusicIndex].src}
+                    autoPlayAfterSrcChange={true}
+                    showSkipControls={true}
+                    showJumpControls={false}
+                    src={playlist[this.state.currentMusicIndex].src}
+                    onClickPrevious={this.handleClickPrevious}
+                    onClickNext={this.handleClickNext}
+                    
+                />
 
             </div>
          
